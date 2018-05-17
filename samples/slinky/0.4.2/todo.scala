15d4b3c447e5cd535072881bac287dc2122604a9
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-05-16 17:53:33 -0700

    Update Slinky version and add example

---
+++ samples/slinky/0.4.2/todo.scala
@@ -0,0 +1,71 @@
+import fiddle.Fiddle, Fiddle.println
+import scalajs.js
+
+@js.annotation.JSExportTopLevel("ScalaFiddle")
+object ScalaFiddle {
+  // $FiddleStart
+  import slinky.core._
+  import slinky.web.html._
+  import scala.scalajs.js
+  import org.scalajs.dom.raw.{Event, HTMLInputElement}
+  
+  case class TodoItem(text: String, id: Long)
+  
+  object TodoList extends StatelessComponentWrapper {
+    case class Props(items: Seq[TodoItem])
+
+    class Def(jsProps: js.Object) extends Definition(jsProps) {
+      override def render() = {
+        ul(props.items.map { item =>
+          li(key := item.id.toString)(item.text)
+        })
+      }
+    }
+  }
+
+  object TodoApp extends ComponentWrapper {
+    type Props = Unit
+    case class State(items: Seq[TodoItem], text: String)
+
+    class Def(jsProps: js.Object) extends Definition(jsProps) {
+      override def initialState = State(Seq.empty, "")
+
+      def handleChange(e: Event): Unit = {
+        val eventValue = e.target.asInstanceOf[HTMLInputElement].value
+        setState(_.copy(text = eventValue))
+      }
+
+      def handleSubmit(e: Event): Unit = {
+        e.preventDefault()
+
+        if (state.text.nonEmpty) {
+          val newItem = TodoItem(
+            text = state.text,
+            id = Date.now().toLong
+          )
+
+          setState(prevState => {
+            State(
+              items = prevState.items :+ newItem,
+              text = ""
+            )
+          })
+        }
+      }
+
+      override def render() = {
+        div(
+          h3("TODO"),
+          TodoList(TodoList.Props(items = state.items)),
+          form(onSubmit := handleSubmit _)(
+            input(onChange := handleChange _, value := state.text),
+            button(s"Add #${state.items.size + 1}")
+          )
+        )
+      }
+    }
+  }
+
+  ReactDOM.render(TodoApp(), Fiddle.panel)
+  // $FiddleEnd
+}
